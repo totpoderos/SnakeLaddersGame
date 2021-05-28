80a6e87be9d7ad5080f03b7ef753ae61b8d66e3e
Marcos <marcos.graup@gmail.com>
2021-05-28 13:45:09 +0200

    Make runable as console app

--- SnakeLaddersGame/Program.cs
+++ SnakeLaddersGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using SnakeLaddersApi;
+using SnakeLaddersApi.Domain;
 
 namespace SnakeLaddersGame
 {
@@ -6,7 +8,23 @@ namespace SnakeLaddersGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var snakeLaddersController =
+                new SnakeLaddersController(new Game(new OneToSixDice(new RandomGenerator(1, 6))),
+                    new InputOutputConsole());
+            Console.WriteLine("Welcome");
+            Console.WriteLine("Available commands");
+            Console.WriteLine("PlaceToken: Place token at starting position");
+            Console.WriteLine("Print:      Print current position");
+            Console.WriteLine("Move n:     Move token n spaces");
+            Console.WriteLine("RollDice:   Move token using roll");
+            Console.WriteLine("Status:     Check if player has won");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.Write("Command:> ");
+                snakeLaddersController.ProcessCommand();
+            }
         }
     }
 }
